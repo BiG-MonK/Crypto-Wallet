e2d0fc8ce609b90fd72c749c2898997b30474065
Sergey Sukharev <s-monk-s@mail.ru>
2017-09-29 02:27:28 +0700

    All to One
    All to One

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 $.getJSON("https://api.coinmarketcap.com/v1/ticker/?limit=10", function(json) {
 	console.log(json);
 	$("#test").html(JSON.stringify(json));
+}); 
 //	var html = "<strong>" + "key" + "</strong>: " + "val" + "<br>";
 	//$("#test").html(html);
-}); 
+  
 /*
       var xhr = new XMLHttpRequest();
 
