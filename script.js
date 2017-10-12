e47b9766ed5ad2e453c97bdbfde20af21bb42995
Sergey Sukharev <s-monk-s@mail.ru>
2017-10-12 23:45:44 +0700

    14

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 // ------------ Bitcoin BTC --------------------
 $.getJSON("https://api.coinmarketcap.com/v1/ticker/bitcoin/", function(json) { // ------------ Bitcoin BTC -------------------
   var html = "";
-  var html = "";
+  var html = ""; 
   console.log(json);
   html = "<strong> Rank: " + json[0].rank + "</strong>: " + "<br>";
   html += "<strong>" + json[0].name + " ("+ json[0].symbol + ")" + "</strong>: " + "<br>";
   html += "<strong>" + json[0].price_usd + " USD</strong> " + "<br>";
  $(".bitcoin").html(html);
 }); 
-// ------------ Monero XMR --------------------
+
+// ------------ Курс Фиата RUR/USD RUR/EUR --------------------
 $.getJSON("https://query.yahooapis.com/v1/public/yql?q=select+*+from+yahoo.finance.xchange+where+pair+=+%22USDRUB,EURRUB%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=", function(json) {  // ------------ Monero XMR --------------------
-  var html = "";
-  console.log(json);
-   console.log(json.query.results.rate[0].Rate);
-  html = "<strong>" + json[0].id + "</strong>: " + "<br>";
-  html += "<strong>" + json[0].price_usd + "</strong>: " + "<br>";
- $(".monero").html(html);
+  var USD = "";
+  var EUR = "";
+  USD = "<strong> Курс RUR/USD: " + json.query.results.rate[0].Rate + "</strong>: " + "<br>";
+  $(".USD").html(USD);
+  EUR = "<strong> Курс RUR/EUR: " + json.query.results.rate[1].Rate + "</strong>: " + "<br>";
+  $(".EUR").html(EUR);
 }); 
