1e079e0d94af73b249441070fee6fe65bf7e098c
BiG-MonK <s-monk-s@mail.ru>
2017-10-12 17:14:31 +0700

    Добавил курсы USD\RUR EUR\RUR

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-$.getJSON("https://api.coinmarketcap.com/v1/ticker/?limit=10", function(json) {
-	//var myStr = JSON.stringify(json);
-  //myStr = JSON.parse(myStr);
+// ------------ Bitcoin BTC --------------------
+$.getJSON("https://api.coinmarketcap.com/v1/ticker/bitcoin/", function(json) { // ------------ Bitcoin BTC -------------------
   var html = "";
-  //$("#test").html(JSON.stringify(json));
-  console.log(json[1].price_usd);
-  json.forEach(function(val) {             //перебор каждого элемента в json
-    var keys = Object.keys(val);
-    html += "<div class = 'crypto'>";
-    keys.forEach(function(key) {
-      if (key == 'id'){
-      html += "<strong>" + val[key] + "</strong>: " + "<br>";}
-      if (key == 'price_usd'){
-      html += "<strong>" + val[key] + "</strong>: " + "<br>";}
-    });
-    html += "</div><br>";
-  });
- $(".message").html(html);
- $(".ETH").html(json[1].price_usd);
+  var html = "";
+  console.log(json);
+  html = "<strong> Rank: " + json[0].rank + "</strong>: " + "<br>";
+  html += "<strong>" + json[0].name + " ("+ json[0].symbol + ")" + "</strong>: " + "<br>";
+  html += "<strong>" + json[0].price_usd + " USD</strong> " + "<br>";
+ $(".bitcoin").html(html);
+}); 
+// ------------ Monero XMR --------------------
+$.getJSON("https://query.yahooapis.com/v1/public/yql?q=select+*+from+yahoo.finance.xchange+where+pair+=+%22USDRUB,EURRUB%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=", function(json) {  // ------------ Monero XMR --------------------
+  var html = "";
+  console.log(json);
+   console.log(json.query.results.rate[0].Rate);
+  html = "<strong>" + json[0].id + "</strong>: " + "<br>";
+  html += "<strong>" + json[0].price_usd + "</strong>: " + "<br>";
+ $(".monero").html(html);
 }); 
