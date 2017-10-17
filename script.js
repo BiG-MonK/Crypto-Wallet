a70ded2a9f701988778bc32a3a2557ab7bdcabf9
BiG-MonK <s-monk-s@mail.ru>
2017-10-17 21:12:04 +0700

    to Home

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-var myCoin {
-  
+var coinMining {            //все имеющиеся криптовалюты от майнинга на сейчас
+  bittrex : {
+    zcl : 27.84086542,
+    zec : 2.28645404
+  },
+  poloniex : {
+    dgb : 417.44101617,
+    lbc : 527.5267266
+  },
+  mpoolhub : {
+    dgb : 0,
+    xmr : 0.43234758
+  },
+  suprnova : {
+    lbc : 0,
+    zcl : 1.6631,
+    zec : 0.5524,
+    xmr : 0.0337
+  },
+  fiat : 6652.93
 }
 // ------------ Bitcoin BTC --------------------
 $.getJSON("https://api.coinmarketcap.com/v1/ticker/bitcoin/", function(json) { // ------------ Bitcoin BTC -------------------
@@ -9,7 +27,7 @@ $.getJSON("https://api.coinmarketcap.com/v1/ticker/bitcoin/", function(json) { /
   html = "<strong> Rank: " + json[0].rank + "</strong>: " + "<br>";
   html += "<strong>" + json[0].name + " ("+ json[0].symbol + ")" + "</strong>: " + "<br>";
   html += "<strong>" + json[0].price_usd + " USD</strong> " + "<br>";
- $(".bitcoin").html(html);
+  $(".bitcoin").html(html);
 }); 
 
 // ------------ Курс Фиата RUR/USD RUR/EUR --------------------
