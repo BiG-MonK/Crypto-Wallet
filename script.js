c2d48c01a48044ead7b1942cb5a6f966faf4c7b0
Sergey Sukharev <s-monk-s@mail.ru>
2017-10-22 23:40:00 +0700

    on Work

--- script.js
+++ script.js
@@ -12,25 +12,30 @@ window.onload = function(){
     secLast = secLast - (minLast * 60);                // Остатки секунд после вычета минут
     time += "<strong>Данные устарели на: " + minLast + ":" + secLast + "</strong> " + "<br>";
     $(".time").html(time);
-    $(".trade.bittrex").html("zec: " + (zecUsd * coinMining.bittrex.zec).toFixed(2) + "<br>" + "zcl: " + (zecUsd * coinMining.bittrex.zec).toFixed(2) + "<br>");
-    $(".trade.poloniex").html("dgb: " + (zecUsd * coinMining.bittrex.zec).toFixed(2) + "<br>" + "lbc: " + (zecUsd * coinMining.bittrex.zec).toFixed(2) + "<br>");
-    $(".pool.mpoolhub").html("dgb: " + 0 + "<br>" + "xmr: " + (zecUsd * coinMining.bittrex.zec).toFixed(2) + "<br>");
-    $(".pool.suprnova").html("lbc: " + (zecUsd * coinMining.bittrex.zec).toFixed(2) + "<br>"
-     + "zcl: " + (zecUsd * coinMining.bittrex.zec).toFixed(2) + "<br>"
-     + "zec: " + (zecUsd * coinMining.bittrex.zec).toFixed(2) + "<br>"
-     + "xmr: " + (zecUsd * coinMining.bittrex.zec).toFixed(2) + "<br>");
+    $(".mining.bittrex").html("zec: " + (zecUsd * coinMining.bittrex.zec).toFixed(2) + "<br>" + "zcl: " + (zclUsd * coinMining.bittrex.zcl).toFixed(2) + "<br>");
+    $(".mining.poloniex").html("dgb: " + (dgbUsd * coinMining.poloniex.dgb).toFixed(2) + "<br>" + "lbc: " + (lbcUsd * coinMining.poloniex.lbc).toFixed(2) + "<br>");
+    $(".mining.mpoolhub").html("dgb: " + 0 + "<br>" + "xmr: " + (dgbUsd * coinMining.mpoolhub.dgb).toFixed(2) + "<br>");
+    $(".mining.suprnova").html("lbc: " + (lbcUsd * coinMining.suprnova.lbc).toFixed(2) + "<br>"
+     + "zcl: " + (zclUsd * coinMining.suprnova.zcl).toFixed(2) + "<br>"
+     + "zec: " + (zecUsd * coinMining.suprnova.zec).toFixed(2) + "<br>"
+     + "xmr: " + (xmrUsd * coinMining.suprnova.xmr).toFixed(2) + "<br>");
     window.setTimeout(arguments.callee, 1000);
   })();
 };
 
-//-----------------  Объект хранящий все имеющиеся криптовалюты от майнинга на сейчас
 var secUpdateGL = 0;              // Глобальная переменная хранящая в дальнейшем разницу в сек после обновления данных по монетам
-var zecUsd = 0;                   // Глобальная переменная хранящая цену ZEC в USD
+var btcUsd = 0;                   // Глобальная переменная хранящая цену BTC в USD
+var xmrUsd = 0;
+var dgbUsd = 0;
+var zclUsd = 0;
+var ethUsd = 0;
+var xrpUsd = 0;
+var zecUsd = 0;
+var lbcUsd = 0;          
 var rur_usd = 0;                  // Глобальная переменная хранящая курс RUR/USD
 var rur_eur = 0;                  // Глобальная переменная хранящая курс RUR/EUR
-var btcUsd = 0;                  
-var rurUsd = 0;                   
-var coinMining = {
+//-----------------  Объект хранящий все имеющиеся криптовалюты от майнинга на сейчас
+var coinMining = { 
   bittrex : {
     zcl : 27.84086542,
     zec : 2.83596232
@@ -53,21 +58,30 @@ var coinMining = {
   dateIns : "18.10.2017 11:00"
 };
 
-// ------------ Bitcoin BTC --------------------
-$.getJSON("https://api.coinmarketcap.com/v1/ticker/?limit=400", function(json) { // ------------ Bitcoin BTC -------------------
+// ------------ JSON запрос данных по крипте --------------------
+$.getJSON("https://api.coinmarketcap.com/v1/ticker/?limit=400", function(json) {
   var html = "";
   var marketCap = "";                               // Переменная для разбиения на разряды большого числа
   secUpdateGL = json[0].last_updated;
-  btcUsd = json[0].price_usd;
-  for (var i = 0; i < 400; i++) {
+  for (var i = 0; i < 400; i++) {                   // 400 первых самых инвестированных криптовалют мира
     switch (json[i].symbol) {
       case "BTC":
+      case "ETH":
       case "XMR":
       case "ZCL":
       case "LBC":
       case "ZEC":
       case "DGB":
       case "XRP":
+      if (json[i].symbol == "BTC") { btcUsd = json[i].price_usd;
+      } else if (json[i].symbol == "ETH") {ethUsd = json[i].price_usd;
+      } else if (json[i].symbol == "XRP") {xrpUsd = json[i].price_usd;
+      } else if (json[i].symbol == "XMR") {xmrUsd = json[i].price_usd;
+      } else if (json[i].symbol == "ZEC") {zecUsd = json[i].price_usd;
+      } else if (json[i].symbol == "DGB") {dgbUsd = json[i].price_usd;
+      } else if (json[i].symbol == "LBC") {lbcUsd = json[i].price_usd;
+      } else if (json[i].symbol == "ZCL") {zclUsd = json[i].price_usd;
+      };
       marketCap = json[i].market_cap_usd.replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ');
       html = $("." + json[i].id).html();
       html += "<strong class='name'> " + json[i].name + " ("+ json[i].symbol + ")" + "</strong>: <br>";
@@ -75,14 +89,13 @@ $.getJSON("https://api.coinmarketcap.com/v1/ticker/?limit=400", function(json) {
       html += "<strong class='USD'>" + json[i].price_usd + " USD</strong> " + "<br>";
       html += "<strong class='rur_usd'>(" + (json[i].price_usd * rur_usd).toFixed(2) + " RUR)</strong> " + "<br>";
       html += "<strong class='price_btc'>" + json[i].price_btc + " BTC</strong> " + "<br>";
-      $("." + json[i].id).html(html);
-
+      $(".left." + json[i].id).html(html);
       html = "";
-      html += "<strong class='market_cap'>Market Cap: <br>" + marketCap + " USD</strong> <br>";
+      html += "Market Cap: <br><strong class='market_cap'>" + marketCap + " USD</strong> <br><br>";
       html += "1h: <strong class='change_coin'> " + json[i].percent_change_1h + "%</strong> " + "<br>";
       html += "24h: <strong class='change_coin'> " + json[i].percent_change_24h + "%</strong> " + "<br>";
       html += "Week: <strong class='change_coin'> " + json[i].percent_change_7d + "%</strong> " + "<br>";
-      $("." + json[i].id + "_rest").html(html);
+      $(".right." + json[i].id).html(html);
     }
   }
 }); 
